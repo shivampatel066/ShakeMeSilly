49777a1aec92e4fe70e0886f98ae3fbbbc363d2c
Shivam Patel <shivampatel066@gmail.com>
2019-04-02 17:25:34 +0530

    Completed UI as well as motion logic but not yet added grade according to result !!

--- ShakeMeSilly/ViewController.swift
+++ ShakeMeSilly/ViewController.swift
@@ -10,11 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    
+    var timer = Timer()
+    
+    var timeInt = 10
+    var scoreInt = 0
+    var imageInt = 1
+    var modeInt = 0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func startGame(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startCounter), userInfo: nil, repeats: true)
+        modeInt = 1
+        startGameButton.isEnabled = false
+        startGameButton.alpha = 0.25
+        startGameButton.setTitle("Shake Me", for: UIControl.State.normal)
+        
+        
+    }
+    
+    @objc func startCounter() {
+        
+        timeInt -= 1
+        timeLabel.text = String(timeInt)
+        if timeInt < 1 {
+            
+            timer.invalidate()
+            timeInt = 10
+            scoreInt = 0
+            modeInt = 0
+            startGameButton.alpha = 1
+            startGameButton.isEnabled = true
+            startGameButton.setTitle("Restart", for: UIControl.State.normal)
+        }
+        
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            
+            if modeInt == 1 {
+                scoreInt += 1
+                scoreLabel.text = String(scoreInt)
+                
+                imageInt += 1
+                
+                if imageInt == 13 {
+                    imageInt = 1
+                }
+                imageView.image = UIImage(named: "Face\(imageInt)")
+            }
+            
+        }
+    }
+    
+    
 }
 
